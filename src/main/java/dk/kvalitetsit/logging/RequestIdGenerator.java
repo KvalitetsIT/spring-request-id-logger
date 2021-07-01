f450083b01d2c17efa04ee60c1a70a1f5c13e869
Jonas Pedersen <jpe@kvalitetsit.dk>
2021-07-01 11:06:58 +0200

    Mark spring-boot-starter-test with test scope.

--- src/main/java/dk/kvalitetsit/logging/RequestIdGenerator.java
+++ src/main/java/dk/kvalitetsit/logging/RequestIdGenerator.java
@@ -1,5 +1,10 @@
 package dk.kvalitetsit.logging;
 
 public interface RequestIdGenerator {
+    /**
+     * Returns the request id. If @customRequestId have been called that one is used. Otherwise the configured HTTP
+     * header is checked and if header not found a random request id is generated.
+     * @return The request id.
+     */
     String getOrGenerateRequestId();
 }
