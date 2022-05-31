e613fb53fbb2ba311ec6cfc3ba3c11d1300f385c
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-05-31 09:51:13 +0200

    Update maven docker image to Java 17.

--- build/maven-run.sh
+++ build/maven-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Build inside docker container
-docker run -e GITHUB_TOKEN=$GITHUB_TOKEN -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src -v $HOME/.m2:/root/.m2 maven:3-jdk-11 /src/build/maven-deploy.sh
+docker run -e GITHUB_TOKEN=$GITHUB_TOKEN -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src -v $HOME/.m2:/root/.m2 maven:3-ibm-semeru-17-focal /src/build/maven-deploy.sh
