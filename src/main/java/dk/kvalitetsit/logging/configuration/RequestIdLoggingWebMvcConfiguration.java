ac2d910a2a56134de78fcef935601a58c6dfaf57
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-07-01 19:27:27 +0200

    Correctly auto configure interceptor. (#3)

--- src/main/java/dk/kvalitetsit/logging/configuration/RequestIdLoggingWebMvcConfiguration.java
+++ src/main/java/dk/kvalitetsit/logging/configuration/RequestIdLoggingWebMvcConfiguration.java
@@ -1,6 +1,8 @@
 package dk.kvalitetsit.logging.configuration;
 
 import dk.kvalitetsit.logging.LoggingInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,11 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class RequestIdLoggingWebMvcConfiguration implements WebMvcConfigurer {
+    private static final Logger logger = LoggerFactory.getLogger(RequestIdLoggingWebMvcConfiguration.class);
+
     @Autowired
     private LoggingInterceptor loggingInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        logger.debug("Adding request id logging interceptor.");
         registry.addInterceptor(loggingInterceptor);
     }
 }
