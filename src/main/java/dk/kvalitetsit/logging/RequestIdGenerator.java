f3bb8eff1798c8454da5e010f2155c843d0794b8
Jonas Pedersen <jpe@kvalitetsit.dk>
2024-09-24 09:11:16 +0200

    Correct minor code smell.

--- src/main/java/dk/kvalitetsit/logging/RequestIdGenerator.java
+++ src/main/java/dk/kvalitetsit/logging/RequestIdGenerator.java
@@ -2,7 +2,7 @@ package dk.kvalitetsit.logging;
 
 public interface RequestIdGenerator {
     /**
-     * Returns the request id. If @customRequestId have been called that one is used. Otherwise the configured HTTP
+     * Returns the request id. If @customRequestId have been called that one is used. Otherwise, the configured HTTP
      * header is checked and if header not found a random request id is generated.
      * @return The request id.
      */
